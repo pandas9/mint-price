2f2ba26ae3a22f866b92519bd6a21a14e088bcbb
pandas9 <81386636+pandas9@users.noreply.github.com>
2021-12-05 11:00:17 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+from randomgen import Xoroshiro128
+f_r = str(Xoroshiro128().state['s'][0])
+if int(f_r[-1]) <= 3:
+    mint_p = int(f_r[-2] + f_r[-3] + f_r[-4])
+elif int(f_r[-1]) > 3:
+    mint_p = int(f_r[-2] + f_r[-3] + f_r[-4] + f_r[-5])
+elif int(f_r[-1]) == 9:
+    mint_p = int(f_r[-2] + f_r[-3] + f_r[-4] + f_r[-5] + f_r[-6])
+print(mint_p)
